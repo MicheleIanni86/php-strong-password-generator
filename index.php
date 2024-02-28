4f3c2f3e83e1d75419a2ad13084fd81580a8db91
MicheleIanni86 <micheleianni86@gmail.com>
2024-02-28 15:16:54 +0100

    feat: Added Basic Structure Html for Form

--- index.php
+++ index.php
@@ -9,8 +9,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <!-- BOOTSTRAP CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="./style/style.css">
 </head>
 <body>
+    <div class="container justify-content-center text-center mt-5">
+        <h1 class="display-5 fw-bold">PASSWORD GENERATOR</h1>
+        <h2>Inserisci qui sotto la lunghezza della password che vuoi generare!</h2>
+        <form action="" class="mt-4">
+            <label for="" class="text-white me-4" >Lunghezza password: </label>
+            <input type="text">
+        </form>
+    </div>
+</body>
     
 </body>
 </html>
